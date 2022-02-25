7f61155c4ad43cb1f60ce8121d20be55ad5e3200
Alexander Holovin <dvjmixxxer@gmail.com>
2022-02-25 15:34:34 +0300

    Add debug

--- hass-addon-petoneer-smartdot/src/BluetoothController.js
+++ hass-addon-petoneer-smartdot/src/BluetoothController.js
@@ -28,6 +28,8 @@ module.exports.BluetoothController = class BluetoothController {
         noble.on('warning', console.log);
 
         noble.on('discover', async (peripheral) => {
+            console.log(peripheral);
+
             if (AUTO_DETECTION_NAMES.includes(peripheral.advertisement?.localName)) {
                 console.log(peripheral.advertisement?.localName);
                 this.peripheral = peripheral;
