c6009aef7550bdf1e9c1bf57a5753b3c7c54f77d
Alexander Holovin <dvjmixxxer@gmail.com>
2022-02-25 14:49:23 +0300

    fix?

--- hass-addon-petoneer-smartdot/src/BluetoothController.js
+++ hass-addon-petoneer-smartdot/src/BluetoothController.js
@@ -1,6 +1,7 @@
 const noble = require('@abandonware/noble');
 
-const serviceUuid = 'fff0';
+// const serviceUuid = 'fff0';
+const serviceUuid = 'A201';
 const commands = {
     stop: '0f0407000008',
     preset_small: '0f0405000107',
@@ -14,9 +15,11 @@ module.exports.BluetoothController = class BluetoothController {
     constructor() {
         this.initialize();
     }
+
     async initialize() {
         noble.on('stateChange', async (state) => {
             console.log(state);
+
             if (state === 'poweredOn') {
                 await noble.startScanningAsync([serviceUuid]);
             }
@@ -26,11 +29,12 @@ module.exports.BluetoothController = class BluetoothController {
 
         noble.on('discover', async (peripheral) => {
             if (AUTO_DETECTION_NAMES.includes(peripheral.advertisement?.localName)) {
-                // console.log(peripheral.advertisement?.localName);
+                console.log(peripheral.advertisement?.localName);
                 this.peripheral = peripheral;
             }
         });
     }
+    
     async sendPresetCommand(command, stayConnected = false) {
         return new Promise(async (resolve, reject) => {
             try {
@@ -38,24 +42,32 @@ module.exports.BluetoothController = class BluetoothController {
                     console.log('no peripheral available');
                     return;
                 }
+
                 noble.reset();
+
                 if (!this.peripheral.mtu) {
                     await this.peripheral.connectAsync();
                 }
+
                 const services = await this.peripheral.discoverServicesAsync([serviceUuid]);
                 const movementService = services[0];
                 const characteristics = await movementService.discoverCharacteristicsAsync(['fff3']);
                 const movementCharacteristic = characteristics[0];
                 const stringCommand = commands[command];
+
                 if (!stringCommand) {
                     await this.peripheral.disconnectAsync();
                     throw new Error(`command "${command}" not recognized.`);
                 }
+
                 const buffer = Buffer.from(stringCommand, "hex");
                 const result = await movementCharacteristic.writeAsync(buffer, false);
-                // console.log({ command, stringCommand, buffer, result });
+                
+                console.log({ command, stringCommand, buffer, result });
+                
                 await this.peripheral.disconnectAsync();
                 resolve(true);
+
             } catch (error) {
                 console.log(error)
                 await this.peripheral.disconnectAsync();
